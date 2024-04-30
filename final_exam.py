212f0212a7ba94fdd4f45e65b26e9075a1281c9b
andreaarrivillaga <163476092+andreaarrivillaga@users.noreply.github.com>
2024-04-30 13:54:45 -0400

    Update final_exam.py

--- final_exam.py
+++ final_exam.py
@@ -6,9 +6,12 @@ st.title("Final Exam, Question 15")
 
 
 url = 'https://drive.google.com/file/d/133D1c5gIVMSogmFb502Rmg34Uj_Q6pTg'
+st.write(f"trying original\n {url}")
+
+
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 
-st.title(f"trying {url}")
+st.write(f"trying new\n {url}")
 
 df=pd.read_csv(url)
 
