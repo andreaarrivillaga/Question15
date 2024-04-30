e40fe6e817fbeaa2c09c99d959f56073cae2f20d
andreaarrivillaga <163476092+andreaarrivillaga@users.noreply.github.com>
2024-04-30 13:38:44 -0400

    Update final_exam.py

--- final_exam.py
+++ final_exam.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+DATA_URL = ('athlete_events.csv.gz')
 st.title("Final Exam, Question 15")
 
-df=read_csv("athlete_events.csv.gz")
+df=read_csv(DATA_URL)
 
 st.title("Done")
