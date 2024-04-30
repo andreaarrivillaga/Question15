913fa51af52e00e741c8f5556e9d99bb7707d9f2
andreaarrivillaga <163476092+andreaarrivillaga@users.noreply.github.com>
2024-04-30 13:46:42 -0400

    Update final_exam.py

--- final_exam.py
+++ final_exam.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-url = 'https://github.com/andreaarrivillaga/Question15/blob/main/athlete_events.csv.gz'
+url = 'https://raw.githubusercontent.com/andreaarrivillaga/Question15/blob/main/athlete_events.csv.gz'
 st.title("Final Exam, Question 15")
 
 df=pd.read_csv(url)
