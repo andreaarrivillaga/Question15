85ad9f35a42ee58e4f870588172e07edc880ddbc
andreaarrivillaga <163476092+andreaarrivillaga@users.noreply.github.com>
2024-04-30 13:13:56 -0400

    Update final_exam.py

--- final_exam.py
+++ final_exam.py
@@ -9,7 +9,7 @@ st.title("Final Exam, Question 15")
 # Load the dataset
 @st.cache_data
 def load_data():
-    df = pd.read_csv("athlete_events.csv")
+    df = pd.read_csv("athlete_events.csv.gz")
     return df
 
 df = load_data()
