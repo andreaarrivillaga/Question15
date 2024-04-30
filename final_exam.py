b392fd2b9732ef28297bce425712330fa0fb2d6a
andreaarrivillaga <163476092+andreaarrivillaga@users.noreply.github.com>
2024-04-30 13:56:02 -0400

    Update final_exam.py

--- final_exam.py
+++ final_exam.py
@@ -7,6 +7,7 @@ st.title("Final Exam, Question 15")
 
 url = 'https://drive.google.com/file/d/133D1c5gIVMSogmFb502Rmg34Uj_Q6pTg'
 st.write(f"trying original\n {url}")
+st.write(f"snp is {url.split('/')[-2]}")
 
 
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
